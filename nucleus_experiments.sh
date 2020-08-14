c520328ec1022be7f7b223d3d680ce11d318ce5e
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-08-13 17:54:56 -0700

    running .sh for experiments

---
+++ nucleus_experiments.sh
@@ -0,0 +1,5 @@
+!/bin/bash
+python lightning_unet.py -f nucleus -e nucleus_ir -i ir -g 3 -l 5e-6 -m 100 -t nucleus,itype
+python lightning_unet.py -f nucleus -e nucleus_hsv -i hsv -g 3 -l 5e-6 -m 100 -t nucleus,itype
+python lightning_unet.py -f nucleus -e nucleus_hsv_with_ir -i hsv_with_ir -g 3 -l 5e-6 -m 100 -t nucleus,itype
+python lightning_unet.py -f nucleus -e nucleus_veg_index -i veg_index -g 3 -l 5e-6 -m 100 -t nucleus,itype
