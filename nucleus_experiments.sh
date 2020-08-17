55e5c1f7e9784015158d594de5546cfdff043a55
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-08-17 10:58:43 -0700

    update experiment scripts

--- nucleus_experiments.sh
+++ nucleus_experiments.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-python lightning_unet.py -f nucleus -e nucleus_fullchannel -i full_channel -g 3 -l 5e-6 -m 50 -t nucleus,itype,precision
-python lightning_unet.py -f nucleus -e nucleus_fullchannel_16bit -i full_channel -g 3 -l 5e-6 -m 50 -t nucleus,itype,precision -r 16
-python lightning_unet.py -f nucleus -e nucleus_rgb -i rgb -g 3 -l 5e-6 -m 50 -t nucleus,itype
-python lightning_unet.py -f nucleus -e nucleus_ir -i ir -g 3 -l 5e-6 -m 50 -t nucleus,itype
-python lightning_unet.py -f nucleus -e nucleus_hsv -i hsv -g 3 -l 5e-6 -m 50 -t nucleus,itype
-python lightning_unet.py -f nucleus -e nucleus_hsv_with_ir -i hsv_with_ir -g 3 -l 5e-6 -m 50 -t nucleus,itype
-python lightning_unet.py -f nucleus -e nucleus_veg_index -i veg_index -g 3 -l 5e-6 -m 50 -t nucleus,itype
+python lightning_unet.py -f nucleus -e nucleus_fullchannel -i full_channel -g 3 -l 5e-6 -m 25 -t nucleus,itype,precision
+python lightning_unet.py -f nucleus -e nucleus_fullchannel_16bit -i full_channel -g 3 -l 5e-6 -m 25 -t nucleus,itype,precision -r 16
+python lightning_unet.py -f nucleus -e nucleus_rgb -i rgb -g 3 -l 5e-6 -m 25 -t nucleus,itype
+python lightning_unet.py -f nucleus -e nucleus_ir -i ir -g 3 -l 5e-6 -m 25 -t nucleus,itype
+python lightning_unet.py -f nucleus -e nucleus_hsv -i hsv -g 3 -l 5e-6 -m 25 -t nucleus,itype
+python lightning_unet.py -f nucleus -e nucleus_hsv_with_ir -i hsv_with_ir -g 3 -l 5e-6 -m 25 -t nucleus,itype
+python lightning_unet.py -f nucleus -e nucleus_veg_index -i veg_index -g 3 -l 5e-6 -m 25 -t nucleus,itype
 
 
 #python lightning_unet.py -f nucleus -e nucleus_fullchannel -i full_channel -g 3 -l 5e-6 -m 5 -t nucleus,itype,precision
