971e9db3063f806c01fb447245717afddf4bf7c4
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-08-26 15:37:50 -0700

    Update lightning_unet.py

--- lightning_unet.py
+++ lightning_unet.py
@@ -82,7 +82,7 @@ class LitUNet(pl.LightningModule):
         # res = get_category(output)
         # return res
         values, indices = torch.max(self.model(x), 1)
-        indices = torch.FloatTensor(indices)
+        indices.type(torch.FloatTensor)
         indices.requires_grad = True
         #print(indices)
         return indices
