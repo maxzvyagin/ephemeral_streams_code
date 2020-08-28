b2579c53d6e616ffc5454852412a7f8e6a436cf7
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-08-28 10:23:48 -0700

    Update lightning_unet_selftraining.py

--- lightning_unet_selftraining.py
+++ lightning_unet_selftraining.py
@@ -121,7 +121,7 @@ if __name__ == "__main__":
     while model.test_loss > 0.05:
         # get a new random set of unlabelled data each time
         print("Getting a new set of unlabelled data...")
-        unlabelled = preprocess.UnlabelledGISDataset(f, IMAGE_TYPE, LARGE_IMAGE, num_images=1000)
+        unlabelled = preprocess.UnlabelledGISDataset(f, IMAGE_TYPE, LARGE_IMAGE, num_images=500)
         print("Iteration {}...".format(i))
         all_data = model.original_train_set
         new_data = []
