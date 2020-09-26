41d67de727133b12fbbac23510a5f6d1a0784e47
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-09-25 18:21:52 -0700

    Update optimization_experiment.py

--- optimization_experiment.py
+++ optimization_experiment.py
@@ -75,9 +75,9 @@ def train_then_test(params):
 
 
 if __name__ == "__main__":
-    hparams = [(10.0**-5, 0.0),  # learning_rate
+    hparams = [(10.0**-8, 10.0**-1),  # learning_rate
                (0.0, 1.0),  # dropout
-               (10.0**-5, 0.0),  # weight decay
+               (10.0**-8, 10.0**-1),  # weight decay
                (1, 12)]  # encoder depth
 
     hyperdrive(objective=train_then_test,
