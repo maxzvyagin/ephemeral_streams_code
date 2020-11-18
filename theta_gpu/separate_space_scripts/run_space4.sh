6bd16d62dedf20867fb7a2d2d4e63aefa2f5e1a5
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-11-17 20:23:37 -0800

    fixing scripts

--- theta_gpu/separate_space_scripts/run_space4.sh
+++ theta_gpu/separate_space_scripts/run_space4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 singularity shell --nv -B /lus:/lus /lus/theta-fs0/software/thetagpu/nvidia-containers/tensorflow2/tf2_20.10-py3.simg
-cd ~/hyper_resilient/theta_gpu
-python ~/hyper_resilient/theta_gpu/theta_running.py -m run -s 6
-python ~/hyper_resilient/theta_gpu/theta_running.py -m run -s 7
+cd ~/ephemeral_streams_code/theta_gpu
+python ~/ephemeral_streams_code/theta_gpu/theta_running.py -m run -s 6
+python ~/ephemeral_streams_code/theta_gpu/theta_running.py -m run -s 7
