a9ae8f466fb3d88c773c46fc06ec09bd58726bd7
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-10-18 12:07:28 -0700

    just learning rate

--- lambda_gpu/pytorch_unet.py
+++ lambda_gpu/pytorch_unet.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-b', '--batch_size', default=64)
     args = parser.parse_args()
-    test_config = {'batch_size': args.batch_size, 'learning_rate': .0001, 'epochs': 25}
+    test_config = {'batch_size': args.batch_size, 'learning_rate': .000001, 'epochs': 25}
     acc, model = segmentation_pt_objective(test_config)
     torch.save(model, "/tmp/mzvyagin/ephemeral_streams_model.pkl")
     # torch.save(model, "initial_model.pkl")
