138c38bc2b99cf1a7b30c373b35d9c88cc06fd7c
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-10-22 11:14:40 -0700

    removing scaling

--- ephemeral_streams/gis_preprocess.py
+++ ephemeral_streams/gis_preprocess.py
@@ -131,9 +131,9 @@ def get_windows(img_f, mask, large_image=False, unlabelled=False, num=500, get_m
     full_image = np.swapaxes(full_image, 1, 2)
 
     # scale values in place
-    for i in range(4):
-        scaler = MinMaxScaler()
-        full_image[i, :, :] = scaler.fit_transform(full_image[i, :, :])
+    # for i in range(4):
+    #     scaler = MinMaxScaler()
+    #     full_image[i, :, :] = scaler.fit_transform(full_image[i, :, :])
 
     max_x = mask.shape[0] // window_size
     max_y = mask.shape[1] // window_size
