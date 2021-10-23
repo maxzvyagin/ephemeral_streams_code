2c27275903ce199b86b3c83d655b13a76c70eaa1
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-10-22 17:53:58 -0700

    Update pytorch_unet.py

--- lambda_gpu/pytorch_unet.py
+++ lambda_gpu/pytorch_unet.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-b', '--batch_size', default=64)
     args = parser.parse_args()
-    test_config = {'batch_size': args.batch_size, 'learning_rate': .00001, 'epochs': 10}
+    test_config = {'batch_size': args.batch_size, 'learning_rate': .001, 'epochs': 10}
     acc, model = segmentation_pt_objective(test_config)
     torch.save(model, "/tmp/mzvyagin/ephemeral_streams_model.pkl")
     # torch.save(model, "initial_model.pkl")
