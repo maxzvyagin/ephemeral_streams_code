947d087c12f062d9e78b4d94e6e81f7e87cab421
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-11-01 22:20:48 -0600

    learning rate

--- lambda_gpu/pytorch_unet.py
+++ lambda_gpu/pytorch_unet.py
@@ -180,7 +180,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-b', '--batch_size', default=64)
     args = parser.parse_args()
-    test_config = {'batch_size': args.batch_size, 'learning_rate': .00001, 'epochs': 50}
+    test_config = {'batch_size': args.batch_size, 'learning_rate': .001, 'epochs': 50}
     wandb.init(project='ephemeral_streams', entity='mzvyagin', config=test_config)
     acc, model = segmentation_pt_objective(test_config)
     torch.save(model, "/tmp/mzvyagin/ephemeral_streams_model.pkl")
