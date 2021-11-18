417b5f3d2d40c515d37289a47ad3643163dc5df9
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-11-18 12:38:41 -0700

    Update pytorch_unet.py

--- lambda_gpu/pytorch_unet.py
+++ lambda_gpu/pytorch_unet.py
@@ -35,9 +35,8 @@ import pdb
 
 
 ### definition of PyTorch Lightning module in order to run everything
-class iou_loss(torch.autograd.Function):
-    def forward(self, iou_value):
-        return -torch.log(iou_value)
+def iou_loss(iou_value):
+    return -torch.log(iou_value)
 
 
 def iou(pred, target):
@@ -101,6 +100,7 @@ class PyTorch_UNet(pl.LightningModule):
 
     def training_step_end(self, outputs):
         # only use when  on dp
+        pdb.set_trace()
         output = outputs['forward'].squeeze(1)
         # loss = self.criterion(output, outputs['expected'])
         f1 = self.f1(output, outputs['expected'].int())
@@ -136,7 +136,7 @@ class PyTorch_UNet(pl.LightningModule):
         # iou = self.iou(output, outputs['expected'].int()).detach().cpu()
         # recall = self.recall_metric(output, outputs['expected'].int()).detach().cpu()
         # precision = self.precision_metric(output, outputs['expected'].int()).detach().cpu()
-        logs = {'val_loss': loss, 'val_accuracy': accuracy, 'val_iou': iou_value,
+        logs = {'val_loss': loss.detach().cpu(), 'val_accuracy': accuracy, 'val_iou': iou_value,
                 'val_f1': f1}
         self.log("validation", logs)
         return {'loss': loss, 'logs': logs, 'val_accuracy': accuracy, 'val_iou': iou_value, 'val_f1': f1}
